91c9b239f4c89cde0d19d4cddd0d915533ae705b
Your Name <you@example.com>
2025-12-10 15:15:27 +0900

    chore: remove contact/privacy from header nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -127,12 +127,6 @@ export default function RootLayout({
               <Link href="/blog" className="hover:text-brand-700">
                 블로그
               </Link>
-                <Link href="/contact" className="hover:text-brand-700">
-                  문의
-                </Link>
-                <Link href="/privacy" className="hover:text-brand-700">
-                  개인정보
-                </Link>
               </nav>
             </div>
           </header>
